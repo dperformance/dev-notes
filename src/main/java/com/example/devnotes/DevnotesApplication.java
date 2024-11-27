7f7dcb5e44bad949970cb558c5afb815a0b4d256
dperformance <docyoungs@naver.com>
2024-11-27 13:39:13 +0900

    first commit

---
+++ src/main/java/com/example/devnotes/DevnotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.devnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devnotes/DevnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
