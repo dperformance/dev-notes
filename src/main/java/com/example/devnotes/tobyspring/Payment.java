e2313509bb071a5eacdb29fbe4984445479f5f1f
dperformance <docyoungs@naver.com>
2024-12-20 11:19:16 +0900

    feat: [toby spring] 1. PaymentService

---
+++ src/main/java/com/example/devnotes/tobyspring/Payment.java
@@ -0,0 +1,50 @@
+package com.example.devnotes.tobyspring;
+
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@ToString
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount,
+                   BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+}
---
+++ src/main/java/com/example/devnotes/tobyspring/PaymentService.java
@@ -0,0 +1,18 @@
+package com.example.devnotes.tobyspring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class PaymentService {
+
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
+                LocalDateTime.now());
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
