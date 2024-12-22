394bae53d3bd75d7faed2cd52c1d58941474586a
dperformance <docyoungs@naver.com>
2024-12-22 16:27:07 +0900

    [toby spring boot] 3.서블릿 등록

--- src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
+++ src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
@@ -1,13 +1,28 @@
 package com.example.devnotes.tobyspringboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+
+import java.io.IOException;
 
 public class TobySpringApplication {
 
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello",new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-type","text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 }
