c3001f9e774c246767d96bc14684ba1d7cc80dde
dperformance <docyoungs@naver.com>
2024-12-28 10:44:21 +0900

    [toby spring boot] 4. 서블릿 요청 처리

--- src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
+++ src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
@@ -17,9 +20,13 @@ public class TobySpringApplication {
             servletContext.addServlet("hello",new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setHeader("Content-type","text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    // 요청 부분
+                    String name = req.getParameter("name");
+
+                    // 응답 부분
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
                 }
             }).addMapping("/hello");
         });
