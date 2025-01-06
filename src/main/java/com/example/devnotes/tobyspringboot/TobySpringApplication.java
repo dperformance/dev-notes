b1570cea95d154bd65a5a31eef11b0ff9c922bd2
dperformance <docyoungs@naver.com>
2025-01-06 12:23:18 +0900

    feat: [toby spring boot] 10. DispatcherServlet 으로 전환

--- src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
+++ src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
@@ -1,24 +1,16 @@
 package com.example.devnotes.tobyspringboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class TobySpringApplication {
 
     public static void main(String[] args) {
         // 가장 기본적인 스프링 컨테이너 생성, 빈 수동 등록이 가능하다.
         // 1. 수동 등록 컨테이너 생성
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         // 2. 빈 등록
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
@@ -27,26 +19,35 @@ public class TobySpringApplication {
 
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller",new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        // 4. 빈 사용
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String result = helloController.hello(name); // 매핑
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(result); // 바인딩
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                    new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
+
+        /** DispatcherServlet 으로 전환을 위해 아래 코드는 더 이상 사용하지 않음. */
+//        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+//            servletContext.addServlet("frontcontroller",new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    // 인증, 보안, 다국어, 공통 기능
+//                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//                        String name = req.getParameter("name");
+//
+//                        // 4. 빈 사용
+//                        HelloController helloController = applicationContext.getBean(HelloController.class);
+//                        String result = helloController.hello(name); // 매핑
+//
+//                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+//                        resp.getWriter().println(result); // 바인딩
+//                    } else {
+//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                    }
+//
+//                }
+//            }).addMapping("/*");
+//        });
+
+
         webServer.start();
     }
 }
