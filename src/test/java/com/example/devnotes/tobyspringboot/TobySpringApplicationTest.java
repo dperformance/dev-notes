2da9b40548ec8eb9e8358e711a955d25841e6c0b
dperformance <docyoungs@naver.com>
2025-01-20 13:36:16 +0900

    feat: [toby spring boot] 17. 테스트 코드를 이용한 테스트 TestRestTemplate

---
+++ src/test/java/com/example/devnotes/tobyspringboot/TobySpringApplicationTest.java
@@ -0,0 +1,30 @@
+package com.example.devnotes.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TobySpringApplicationTest {
+
+    @Test
+    void api() {
+        // http localhost:8080/hello?name=spring
+        TestRestTemplate rest = new TestRestTemplate();
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // 응답 검증 3가지
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        assertThat(res.getBody().trim()).isEqualTo("Hello Spring");
+    }
+
+}
