de3dc736e4e51cf0603b4abdf056a2e63d71a244
dperformance <docyoungs@naver.com>
2025-01-20 16:04:42 +0900

    feat: [toby spring boot] 20. 메타 애노테이션과 합성 애노테이션

--- src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
+++ src/main/java/com/example/devnotes/tobyspringboot/TobySpringApplication.java
@@ -1,25 +1,22 @@
 package com.example.devnotes.tobyspringboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+//@Configuration  // MySpringBootAnnotation 이동
+//@ComponentScan  // MySpringBootAnnotation 이동
+@MySpringBootAnnotation
 public class TobySpringApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
 
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
+    /** Config 로 이동 */
+//    @Bean
+//    public ServletWebServerFactory servletWebServerFactory() {
+//        return new TomcatServletWebServerFactory();
+//    }
+//
+//    @Bean
+//    public DispatcherServlet dispatcherServlet() {
+//        return new DispatcherServlet();
+//    }
 
 
     public static void main(String[] args) {
