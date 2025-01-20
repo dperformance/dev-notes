7e24ed536b5b41738944d603095b0c5de367da42
dperformance <docyoungs@naver.com>
2025-01-20 17:35:23 +0900

    feat: [toby spring boot] 22. 동적인 자동 구성 정보 등록

--- src/main/java/com/example/devnotes/tobyspringboot/config/Config.java
+++ src/main/java/com/example/devnotes/tobyspringboot/config/Config.java
@@ -2,6 +2,9 @@ package com.example.devnotes.tobyspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 인프라 빈 구성 정보의 분리로 인한 미사용 클래스
+ */
 @Configuration
 public class Config {
 
--- src/main/java/com/example/devnotes/tobyspringboot/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/example/devnotes/tobyspringboot/config/EnableMyAutoConfiguration.java
@@ -1,7 +1,5 @@
 package com.example.devnotes.tobyspringboot.config;
 
-import com.example.devnotes.tobyspringboot.config.autoconfig.DispatcherServletConfig;
-import com.example.devnotes.tobyspringboot.config.autoconfig.TomcatWebServerConfig;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -11,6 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+//@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) 동적인 자동 구성 정보 등록
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
