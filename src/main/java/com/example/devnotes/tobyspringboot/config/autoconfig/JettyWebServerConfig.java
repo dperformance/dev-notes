92189b9d12912d9ed365fee3ddc26164e7d72807
dperformance <docyoungs@naver.com>
2025-01-22 17:01:25 +0900

    feat: [toby spring boot] 26. @Conditional과 Condition

--- src/main/java/com/example/devnotes/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/example/devnotes/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
@@ -4,12 +4,24 @@ import com.example.devnotes.tobyspringboot.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 //@Configuration
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
